d7c86b1afa487fae1b62c0321b0acb75b7be1afd
Ahsan <muhammad.ahsan@logicmonitor.com>
2020-11-04 01:30:51 +0100

    VM script updated

--- script/vm.sh
+++ script/vm.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-export GCP_PROJECT_ID=$1
-export LM_COMPANY_NAME=$2
-export LM_ACCESS_ID=$3
-export LM_ACCESS_KEY=$4
 
-sudo apt-get update -y &&
-sudo apt-get install -y rubygems build-essential &&
-sudo apt-get install -y ruby-dev &&
-sudo gem install fluentd --no-doc &&
-sudo gem install fluent-plugin-lm-logs &&
-sudo gem install fluent-plugin-gcloud-pubsub-custom && 
-curl  https://raw.githubusercontent.com/logicmonitor/lm-logs-gcp/master/script/fluentd.conf --output template.conf &&
-envsubst < template.conf  | cat > fluentd.conf
-fluentd -c ./fluentd.conf
+
+function deploy_lm-logs {
+	export GCP_PROJECT_ID=$1
+	export LM_COMPANY_NAME=$2
+	export LM_ACCESS_ID=$3
+	export LM_ACCESS_KEY=$4
+
+	sudo apt-get update -y &&
+	sudo apt-get install -y rubygems build-essential &&
+	sudo apt-get install -y ruby-dev &&
+	sudo gem install fluentd --no-doc &&
+	sudo gem install fluent-plugin-lm-logs &&
+	sudo gem install fluent-plugin-gcloud-pubsub-custom && 
+	curl  https://raw.githubusercontent.com/logicmonitor/lm-logs-gcp/master/script/fluentd.conf --output template.conf &&
+	envsubst < template.conf  | cat > fluentd.conf
+	fluentd -c ./fluentd.conf
+}
