7b73135541a870bdf11fbbedc5d13475cea73769
Pooja Choudhary <pooja.choudhary@logicmonitor.com>
2021-04-22 18:32:16 +0530

    user_fix:  Fixing the issue where the conf file fails to run

--- script/vm.sh
+++ script/vm.sh
@@ -8,4 +8,4 @@ sudo gem install fluent-plugin-lm-logs &&
 sudo gem install fluent-plugin-gcloud-pubsub-custom && 
 curl  https://raw.githubusercontent.com/logicmonitor/lm-logs-gcp/master/script/fluentd.conf --output template.conf &&
 envsubst < template.conf  | cat > fluentd.conf
-fluentd -c ./fluentd.conf
+sudo fluentd -c ./fluentd.conf
